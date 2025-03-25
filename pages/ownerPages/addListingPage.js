eed246fd80fa613a1e0728469c69757c46e8dc8a
Hitin Sachdeva <140743627+HitinSachdeva161@users.noreply.github.com>
2025-03-25 09:06:12 -0700

    deleted func not needed

--- pages/ownerPages/addListingPage.js
+++ pages/ownerPages/addListingPage.js
@@ -17,17 +17,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 let uploadedImageUrl = null;
 
 // Camera handler functions
-function dataURLtoFile(dataurl, filename) {
-  const arr = dataurl.split(',');
-  const mime = arr[0].match(/:(.*?);/)[1];
-  const bstr = atob(arr[1]);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-  return new File([u8arr], filename, { type: mime });
-}
+
 console.log("uploadedImageUrl", uploadedImageUrl);
 
 const fileInput = document.getElementById("file-input");
