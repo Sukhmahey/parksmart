9213ddb383f062e8c5a3ff6ab632a583dc8e1af0
ManmeetVirdi17 <mvirdi1798@gmail.com>
2025-04-03 22:01:02 -0700

    offline page
    offline page

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "offline-cache-v2";
+const OFFLINE_URL = "offline.html";
+const ASSETS_TO_CACHE = [
+    OFFLINE_URL,
+    
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request).catch(() => {
+                if (event.request.destination === "document") {
+                    return caches.match(OFFLINE_URL);
+                }
+            });
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
